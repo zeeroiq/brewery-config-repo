5f0d5b8be8db1651c4d90af097dd75978db36f5e
zeero1q <shrikantvishwakrma74@gmail.com>
2020-10-28 02:39:11 +0530

    project initialization files. closes #1

---
+++ src/main/java/com/zeeroiq/configrepo/ConfigRepoApplication.java
@@ -0,0 +1,13 @@
+package com.zeeroiq.configrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigRepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zeeroiq/configrepo/ConfigRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeeroiq.configrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
